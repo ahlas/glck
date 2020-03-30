198ec741b466f15d740522e59b6dd595d286d173
yusuf <yustun355@hotmail.com>
2020-03-30 17:05:58 +0300

    CheckBox hafızası updated

--- gezi/static/script/allScript.js
+++ gezi/static/script/allScript.js
@@ -9,16 +9,22 @@ function handleClick(cb) {
 }
 
 // Sayfanın en kısa yolu bulduktan sonra tekrar update etmesi olayı için sayfa yenileme işlemi yaptırılıyor
-function dynamicButton(url){
+function dynamicButton(url,checkboxList){
     //window.location.href = url;
     //window.location.reload(true);
     var checkedBoxes = getCheckedBoxes("cityChecks[]");
-    if(checkedBoxes.length!=0){
+    if(checkedBoxes.length!=99999){
+        localStorage.setItem("size",checkedBoxes.length.toString());
+        for(var i=0; i<checkedBoxes.length;i++){
+            var val = i.toString();
+            localStorage.setItem(val, checkedBoxes[i]);
+        }
         document.getElementById("xdemR").value="actionState";
         document.getElementById("xdemR1").click();
         return 1;
     }
     else{
+        localStorage.setItem("size","-1");
         return 5;
     }
 
@@ -37,15 +43,16 @@ function pageLoadFinishFunction(){
 function getCheckedBoxes(chkboxName) {
   var checkboxes = document.getElementsByName(chkboxName);
   var checkboxesChecked = [];
+  var unchecked = new Array(99999);
   // loop over them all
   for (var i=0; i<checkboxes.length; i++) {
      // And stick the checked ones onto an array...
      if (checkboxes[i].checked) {
-        checkboxesChecked.push(checkboxes[i]);
+        checkboxesChecked.push(checkboxes[i].value);
      }
   }
   // Return the array if it is non-empty, or null
-  return checkboxesChecked.length > 0 ? checkboxesChecked : null;
+  return checkboxesChecked.length > 0 ? checkboxesChecked : unchecked;
 }
 
 // Call as
